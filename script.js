053d50131eca137ac4683fd3fb8324a883e81cf0
ivan-mangulabnan <ivanmangulabnan19@gmail.com>
2024-07-24 17:22:03 +0800

    Add dynamic changing for grid

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-document.addEventListener(`DOMContentLoaded`, () => {
-    createBox();
+const button = document.querySelector(`button`);
+
+button.addEventListener(`click`, () => {
+
+    let gridCount = Number(prompt(`Enter an integer,`));
+    createBox(gridCount);
+
 })
 
 const container = document.querySelector(`#main`);
 container.textContent = ``;
 
-function createBox() {
-    for(let i = 0; i < 2500; i++) {
+function createBox(gridCount) {
+
+    container.innerHTML = ``;
+
+    for(let i = 0; i < gridCount * gridCount; i++) {
+
+        console.log(gridCount);
         const box = document.createElement(`div`);
-        box.className = `box`;
+        box.classList.add(`grid`);
+        box.style.flex = `0 0 calc(calc(100% / ${gridCount}) - 4px)`;
+        box.style.paddingTop = `calc(calc(100% / ${gridCount}) - 4px)`;
         container.appendChild(box);
     }
 }
